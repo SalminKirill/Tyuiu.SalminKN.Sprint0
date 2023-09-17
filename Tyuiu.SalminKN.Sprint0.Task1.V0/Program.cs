64e3ceb68f4c90b066cd6c9565b341adb06fbeda
Tyuiu.SalminKN@outlook.com <Tyuiu.SalminKN@outlook.com>
2023-09-18 00:00:08 +0500

    Task1

---
+++ Tyuiu.SalminKN.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SalminKN.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            int age = int.MaxValue;
+            string name = string.Empty;
+
+            Console.WriteLine("Hello," + name + "a am "+ age);
+            Console.ReadLine();
+        }
+    }
+}
